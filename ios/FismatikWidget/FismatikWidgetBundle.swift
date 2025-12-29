5088b2afceaa07a798c54963ed3291da882e728d
Squared <mac@192.168.1.47>
2025-12-29 16:03:31 +0300

    feat: Add iOS Widget support and bump version to 94

---
+++ ios/FismatikWidget/FismatikWidgetBundle.swift
@@ -0,0 +1,18 @@
+//
+//  FismatikWidgetBundle.swift
+//  FismatikWidget
+//
+//  Created by Squared on 29.12.2025.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct FismatikWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        FismatikWidget()
+        FismatikWidgetControl()
+        FismatikWidgetLiveActivity()
+    }
+}
